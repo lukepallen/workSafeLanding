72a51d99279ac628475bbbcf95474a52d5dd01c1
Luke Allen <lukepatrickallen@gmail.com>
2019-02-26 18:30:10 -0800

    Updates

---
+++ precache-manifest.cbe056e303d3fa9cf11e61d7c8592e13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a01eeb56dd62d0ef40427b04748df34d",
+    "url": "/workSafeLanding/static/media/placeholder_image2.a01eeb56.jpg"
+  },
+  {
+    "revision": "b6028d25f874342e266fc8cb089a3a90",
+    "url": "/workSafeLanding/static/media/placeholder_image1.b6028d25.jpg"
+  },
+  {
+    "revision": "b3a5c114fcec5099d4cb",
+    "url": "/workSafeLanding/static/js/runtime~main.b3a5c114.js"
+  },
+  {
+    "revision": "55f4dd2a93489f32d275",
+    "url": "/workSafeLanding/static/js/main.55f4dd2a.chunk.js"
+  },
+  {
+    "revision": "fc201e3dc8c21ca8dbe8",
+    "url": "/workSafeLanding/static/js/2.fc201e3d.chunk.js"
+  },
+  {
+    "revision": "55f4dd2a93489f32d275",
+    "url": "/workSafeLanding/static/css/main.ecfc28de.chunk.css"
+  },
+  {
+    "revision": "df0848d324ebf590a2e21578fd279e04",
+    "url": "/workSafeLanding/index.html"
+  }
+];
